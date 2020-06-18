1226020b597da2a10dbd83762ef2d1ec5f37b2fb
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-06-17 19:32:18 -0500

    feat(selectionSort.java): add push method and add numbers

--- 3. Elementary-Sorts/1-SelectionSort.java
+++ 3. Elementary-Sorts/1-SelectionSort.java
@@ -1,35 +1,66 @@
-public class SelectionSort<Item> {
+public class SelectionSort {
 
-  private Item[] array;
+  private int[] array;
+  private int N = 0;
 
-  public static void sort(Item[] array) {
+  public SelectionSort(int capacity) {
+     array = new int[capacity]; 
+  }
+
+  public void sort() {
     int N = array.length;
     for(int i=0; i<N; i++) {
       int min = i;
       for(int j=i+1; j<N; j++)
         if(less(array[j], array[min])) 
           min = j;
-      exch(array, i, min)
+      exch(i, min);
     }
   }
 
-  public Boolean less(Item a, Item b) {
-    return a < b;
-  }
-  
-  public void exch(Item[] array, int index, Item min) {
-    a[index] = min;
+  public void push(int value) {
+    array[N++] = value;
   }
 
-  public Boolean isSorted(){
-    int N = array.length;
+  public Boolean less(int a, int b) {
+    return (a < b);
+  }
+  
+  public void exch(int index, int min) {
+    array[index] = min;
+  }
+
+  public Boolean isSorted() {
     for(int i=0; i<N; i++)
       if(array[i] > array[i+1]) 
         return false;
     return true;
   }
 
-  static void main (String[] args) {
+  public void print() {
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + "-");
+    }
+    System.out.print("\n");
+  }
+
+  public static void main (String[] args) {
+    SelectionSort arr = new SelectionSort(10);
+
+    arr.push(10);
+    arr.push(2);
+    arr.push(4);
+    arr.push(6);
+    arr.push(3);
+    arr.push(8);
+    arr.push(9);
+    arr.push(7);
+    arr.push(1);
+    arr.push(5);
+    arr.print();
+
+    arr.sort();
+    arr.print();
 
   }
 }
