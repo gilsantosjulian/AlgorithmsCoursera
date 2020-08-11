063ccd8ceec4ec5968425bf44a01aa345886ecc2
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-08-10 19:30:03 -0500

    feat(2-Heap): add insert and swim methods

--- 6. Priority Queues/2-Heap.java
+++ 6. Priority Queues/2-Heap.java
@@ -1,26 +1,36 @@
-public class MaxPQ<Key extends Comparable<Key>> {
+class MaxPQ<Key extends Comparable<Key>> {
 
   private Key[] pq;
   private int N;
 
-  public MaxPQ(int capacity)
+  public MaxPQ(int capacity) { 
+    pq = (Key[]) new Comparable[capacity+1];
+   }
 
-  public void insert () {
+  public void insert (Key x) {
     pq[++N] = x;
+    swim(N);
   }
 
-  public static void sort(Comparable[] pq) {
-    int N = pq.length;
-    for(int k = N/2; k >= 1; k--)
-      sink(pq, k, N);
-
-    while(N > 1) {
-      exch(pq, 1, N);
-      sink(pq, 1, --N);
+  private void swim(int k) {
+    while(k > 1 && less(k/2, k)) {
+      exch(k, k/2);
+      k = k/2;
     }
   }
 
-  private static void sink(Comparable[] pq, int k, int N) {
+  public void sort(Comparable[] pq) {
+    int N = pq.length;
+    for(int k = N/2; k >= 1; k--)
+      sink(k, N);
+
+    while(N > 1) {
+      exch(1, N);
+      sink(1, --N);
+    }
+  }
+
+  private void sink(int k, int N) {
     while(2*k <= N) {
       int j = 2 * k;
       if(j < N && less(j, j + 1)) j++;
@@ -31,16 +41,40 @@ public class MaxPQ<Key extends Comparable<Key>> {
 
   }
 
-  private static boolean less(Comparable[] pq, int i, int j) {
-    
+  private boolean less(int i, int j) {
+    return pq[i].compareTo(pq[j]) < 0;
   }
 
-  private static boolean exch(Comparable[] pq, int i, int j) {
-    
+  private void exch(int i, int j) {
+    Key t = pq[i];
+    pq[i] = pq[j];
+    pq[j] = t;
   }
 
+  public void print() {
+    for (int i = 0; i < N; i++) {
+      System.out.print(pq[i] + "-");
+    }
+    System.out.print("\n");
+  }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
+
+    MaxPQ pq = new MaxPQ(11);
+
+    pq.insert("T");
+    pq.insert("S");
+    pq.insert("R");
+    pq.insert("P");
+    pq.insert("N");
+    pq.insert("O");
+    pq.insert("A");
+    pq.insert("E");
+    pq.insert("I");
+    pq.insert("H");
+    pq.insert("G");
+
+    pq.print();
 
   }
 }
